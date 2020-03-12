be80ba1034dfe0bb125e196b6f865678074ef8bc
dmitry.pavlov <dmitry.pavlov@simplinic.com>
2020-03-12 23:42:46 +0300

    recursion-impl

--- src/main/java/recursion/Factorial.java
+++ src/main/java/recursion/Factorial.java
@@ -3,6 +3,12 @@ package recursion;
 public class Factorial {
 
     int of(int base) {
-        throw new UnsupportedOperationException();
+        if (base < 0) {
+            throw new IllegalArgumentException("base should be >= 0");
+        }
+        if (base == 0) {
+            return 1;
+        }
+        return base * of(base - 1);
     }
 }
--- src/main/java/recursion/Fibonacci.java
+++ src/main/java/recursion/Fibonacci.java
@@ -3,6 +3,15 @@ package recursion;
 public class Fibonacci {
 
     int at(int index) {
-        throw new UnsupportedOperationException();
+        if (index < 0) {
+            throw new IllegalArgumentException("index should be >= 0");
+        }
+        if (index == 0 || index == 1) {
+            return index;
+        }
+        if (index == 2) {
+            return 1;
+        }
+        return at(index - 1) + at(index - 2);
     }
 }
