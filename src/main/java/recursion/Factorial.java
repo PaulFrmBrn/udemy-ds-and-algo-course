03c4712b0e45d778323160c9ca5391c1d4548f87
zamonier <eugene.mercuriev@gmail.com>
2020-03-16 23:21:11 +0300

    Add Fibonacci implementation - corner cases

--- src/main/java/recursion/Factorial.java
+++ src/main/java/recursion/Factorial.java
@@ -3,6 +3,9 @@ package recursion;
 public class Factorial {
 
     int of(int base) {
+        if (base < 0) {
+            throw new IllegalArgumentException("base < 0");
+        }
         return base != 0
             ? base * of(base - 1)
             : 1;
