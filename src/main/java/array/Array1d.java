c8c186a5ce08fec22f681a38f7eec029c485c768
Eugene Mercuriev <eugene.mercuriev@gmail.com>
2020-04-13 17:37:17 +0300

    04 array zamonier (#18)
    * [04-Array] Make tests for 1D && 2D array green

--- src/main/java/array/Array1d.java
+++ src/main/java/array/Array1d.java
@@ -2,6 +2,7 @@ package array;
 
 import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.stream.IntStream;
 
 /**
  * One-dimensional array
@@ -10,40 +11,91 @@ import java.util.function.Consumer;
  */
 public class Array1d<T> implements Array<T, Coordinates1d> {
 
+    private final Object[] array;
+
+    private Array1d(int size) {
+        this.array = new Object[size];
+    }
+
     /**
      * Factory method for array creation
      *
      * @param size - array's size
-     * @param <T> - type of values that allow to put into array
+     * @param <T>  - type of values that allow to put into array
      * @return array of the given size
      */
     public static <T> Array1d<T> of(int size) {
-        throw new UnsupportedOperationException();
+        var s = valid(size);
+        return new Array1d<>(s);
     }
 
     @Override
     public T insert(Coordinates1d coordinates, T value) {
-        throw new UnsupportedOperationException();
+        var v = valid(value);
+        var index = valid(coordinates);
+
+        if (array[index] != null) {
+            throw new ArrayStoreException("Cell is already occupied");
+        }
+
+        array[index] = v;
+        return v;
     }
 
     @Override
     public Optional<T> at(Coordinates1d coordinates) {
-        throw new UnsupportedOperationException();
+        var index = valid(coordinates);
+        return Optional.ofNullable((T) array[index]);
     }
 
     @Override
     public Optional<Coordinates1d> search(T value) {
-        throw new UnsupportedOperationException();
+        var v = valid(value);
+
+        for (int index = 0; index < array.length; index++) {
+            if(v.equals(array[index])){
+                return Optional.of(Coordinates1d.of(index));
+            }
+        }
+        return Optional.empty();
     }
 
     @Override
     public void delete(Coordinates1d coordinates) {
-        throw new UnsupportedOperationException();
+        var index = valid(coordinates);
+        array[index] = null;
     }
 
     @Override
     public void visit(Consumer<T> consumer) {
-        throw new UnsupportedOperationException();
+        for (Object o : array) {
+            consumer.accept((T) o);
+        }
+    }
+
+    private static int valid(int size) {
+        if (size < 0) {
+            throw new NegativeArraySizeException();
+        }
+        return size;
+    }
+
+    private int valid(Coordinates1d coordinates) {
+        if (coordinates == null) {
+            throw new IllegalArgumentException(Coordinates1d.class.getSimpleName() + " must be non-null");
+        }
+        var index = coordinates.getIndex();
+        if (index < 0 || index >= array.length) {
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+        return index;
+    }
+
+    private T valid(T value) {
+        if (value == null) {
+            throw new IllegalArgumentException("Value must be non-null");
+        }
+        return value;
     }
 
 }
--- src/main/java/array/Array2d.java
+++ src/main/java/array/Array2d.java
@@ -2,6 +2,7 @@ package array;
 
 import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.stream.IntStream;
 
 /**
  * Two-dimensional array
@@ -10,40 +11,92 @@ import java.util.function.Consumer;
  */
 public class Array2d<T> implements Array<T, Coordinates2d> {
 
+    private Array1d<T>[] array;
+
+    private Array2d(int rows, int columns) {
+        this.array = new Array1d[rows];
+        for (int i = 0; i < rows; i++) {
+            array[i] = Array1d.of(columns);
+        }
+    }
+
     /**
      * Factory method for array creation
      *
-     * @param numberOfRows - number of array's rows
+     * @param numberOfRows    - number of array's rows
      * @param numberOfColumns - number of array's columns
-     * @param <T> - type of values that allow to put into array
+     * @param <T>             - type of values that allow to put into array
      * @return array of the given number of rows and columns
      */
     public static <T> Array2d<T> of(int numberOfRows, int numberOfColumns) {
-        throw new UnsupportedOperationException();
+        var rows = valid(numberOfRows);
+        var columns = valid(numberOfColumns);
+        return new Array2d<>(rows, columns);
     }
 
     @Override
     public T insert(Coordinates2d coordinates, T value) {
-        throw new UnsupportedOperationException();
+        var v = valid(value);
+        var row = valid(coordinates);
+        var column = Coordinates1d.of(coordinates.getColumn());
+        return array[row].insert(column, v);
     }
 
     @Override
     public Optional<T> at(Coordinates2d coordinates) {
-        throw new UnsupportedOperationException();
+        var row = valid(coordinates);
+        var column = Coordinates1d.of(coordinates.getColumn());
+        return array[row].at(column);
     }
 
     @Override
     public Optional<Coordinates2d> search(T value) {
-        throw new UnsupportedOperationException();
+        var v = valid(value);
+        return IntStream
+                .range(0, array.length)
+                .mapToObj(row -> array[row]
+                        .search(v)
+                        .map(column -> Coordinates2d.of(row, column.getIndex())))
+                .flatMap(Optional::stream)
+                .findFirst();
     }
 
     @Override
     public void delete(Coordinates2d coordinates) {
-        throw new UnsupportedOperationException();
+        var row = valid(coordinates);
+        var column = Coordinates1d.of(coordinates.getColumn());
+        array[row].delete(column);
     }
 
     @Override
-    public void visit(Consumer<T> rowConsumer) {
-        throw new UnsupportedOperationException();
+    public void visit(Consumer<T> consumer) {
+        for (Array1d<T> a : array) {
+            a.visit(consumer);
+        }
+    }
+
+    private int valid(Coordinates2d coordinates) {
+        if (coordinates == null) {
+            throw new IllegalArgumentException(Coordinates.class.getSimpleName() + " must be non-null");
+        }
+        var row = coordinates.getRow();
+        if (row < 0 || row >= array.length) {
+            throw new ArrayIndexOutOfBoundsException(row);
+        }
+        return row;
+    }
+
+    private static int valid(int size) {
+        if (size < 0) {
+            throw new NegativeArraySizeException();
+        }
+        return size;
+    }
+
+    private T valid(T value) {
+        if (value == null) {
+            throw new IllegalArgumentException("Value must be non-null");
+        }
+        return value;
     }
 }
