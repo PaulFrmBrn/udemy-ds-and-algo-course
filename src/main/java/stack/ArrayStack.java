2b67f10952055492625dfa15ab65a33a80817410
dmitry.pavlov <dmitry.pavlov@simplinic.com>
2020-05-16 00:26:00 +0300

    06 stack implementation
    06 stack implementation

--- src/main/java/stack/ArrayStack.java
+++ src/main/java/stack/ArrayStack.java
@@ -1,41 +1,64 @@
 package stack;
 
+import java.util.Arrays;
+
+import static java.util.Objects.requireNonNull;
+
 /**
  * @author Dmitry Pavlov
  * @since 15.05.2020
  */
 public class ArrayStack<T> implements Stack<T> {
 
+    private static final int EMPTY_STACK_POINTER = -1;
+
+    private final Object[] array;
+    private int pointer = EMPTY_STACK_POINTER;
+
     public ArrayStack(int size) {
+        this.array = new Object[size];
     }
 
     @Override
     public void push(T value) {
-        throw new UnsupportedOperationException();
+        requireNonNull(value, "value");
+        if (isFull()) {
+            throw new IllegalStateException("Stack is full");
+        }
+        array[++pointer] = value;
     }
 
     @Override
     public T pop() {
-        throw new UnsupportedOperationException();
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        T value = (T) array[pointer];
+        array[pointer--] = null;
+        return value;
     }
 
     @Override
     public T peek() {
-        throw new UnsupportedOperationException();
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return (T) array[pointer];
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException();
+        return pointer == EMPTY_STACK_POINTER;
     }
 
     @Override
     public boolean isFull() {
-        throw new UnsupportedOperationException();
+        return pointer == array.length - 1;
     }
 
     @Override
     public void erase() {
-        throw new UnsupportedOperationException();
+        Arrays.fill(array, null);
+        pointer = -1;
     }
 }
--- src/main/java/stack/LinkedListStack.java
+++ src/main/java/stack/LinkedListStack.java
@@ -1,39 +1,74 @@
 package stack;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * @author Dmitry Pavlov
  * @since 15.05.2020
  */
 public class LinkedListStack<T> implements Stack<T> {
 
+    private Node<T> head;
+
     @Override
     public void push(T value) {
-        throw new UnsupportedOperationException();
+        head = new Node<>(value, head);
     }
 
     @Override
     public T pop() {
-        throw new UnsupportedOperationException();
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        var value = head.getValue();
+        head = head.getNext();
+        return value;
     }
 
     @Override
     public T peek() {
-        throw new UnsupportedOperationException();
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return head.getValue();
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException();
+        return head == null;
     }
 
     @Override
     public boolean isFull() {
-        throw new UnsupportedOperationException();
+        return false;
     }
 
     @Override
     public void erase() {
-        throw new UnsupportedOperationException();
+        head = null;
+    }
+
+    public static class Node<T> {
+
+        private final T value;
+        private Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = requireNonNull(value, "value");
+            this.next = next;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
     }
 
 }
