18e0ed810c5ab2b05594e1ccbaafff6ae0b7c4f7
dmitry.pavlov <dmitry.pavlov@simplinic.com>
2020-06-09 23:24:44 +0300

    08 binary tree implementation

--- src/main/java/tree/binary/ArrayTree.java
+++ src/main/java/tree/binary/ArrayTree.java
@@ -1,7 +1,9 @@
 package tree.binary;
 
+import java.util.NoSuchElementException;
 import java.util.function.Consumer;
 
+import static java.util.Objects.requireNonNull;
 
 /**
  * @author Dmitry Pavlov
@@ -9,41 +11,146 @@ import java.util.function.Consumer;
  */
 public class ArrayTree<T> implements Tree<T> {
 
+    private final Object[] tree;
+    private int lastUsed;
+
     public ArrayTree(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("Size should be greater than 0");
+        }
+        this.tree = new Object[size+1];
+        this.lastUsed = 0;
     }
 
     @Override
     public void insert(T value) {
-        throw new UnsupportedOperationException();
+        requireNonNull(value, "value");
+        if (isFull()) {
+            throw new IllegalStateException("Tree is full");
+        }
+        tree[++lastUsed] = value;
     }
 
     @Override
     public void delete(T value) {
-        throw new UnsupportedOperationException();
+
+        requireNonNull(value, "value");
+        if (isEmpty()) {
+            throw new IllegalStateException("Tree is empty");
+        }
+
+        var indexToDelete = 0;
+        for (int i = 1; i <= lastUsed; i++) {
+            if (value.equals(tree[i])) {
+                indexToDelete = i;
+                break;
+            }
+        }
+
+        if (indexToDelete == 0) {
+            throw new NoSuchElementException("No such element in the tree");
+        }
+
+        if (indexToDelete != lastUsed) {
+            tree[indexToDelete] = tree[lastUsed];
+        }
+        tree[lastUsed--] = null;
+
     }
 
     @Override
     public void traverse(Consumer<T> consumer, TraversalType traversalType) {
-        throw new UnsupportedOperationException();
+
+        requireNonNull(consumer, "consumer");
+        requireNonNull(traversalType, "traversalType");
+
+        switch (traversalType) {
+            case PRE_ORDER: {
+                preOrderTraverse(1, consumer);
+            }
+            break;
+            case IN_ORDER: {
+                inOrderTraverse(1, consumer);
+            }
+            break;
+            case POST_ORDER: {
+                postOrderTraverse(1, consumer);
+            }
+            break;
+            case LEVEL_ORDER: {
+                levelOrderTraverse(consumer);
+            }
+            break;
+            default: {
+                throw new IllegalArgumentException("This type of traversal is not supported");
+            }
+        }
+
     }
 
     public void traverse(Consumer<T> consumer) {
-        throw new UnsupportedOperationException();
+        traverse(consumer, TraversalType.LEVEL_ORDER);
+    }
+
+    void preOrderTraverse(int index, Consumer<T> consumer) {
+        if (index > lastUsed) {
+            return;
+        }
+        consumer.accept((T)tree[index]);
+        preOrderTraverse(index * 2, consumer);
+        preOrderTraverse(index * 2 + 1, consumer);
+    }
+
+    void inOrderTraverse(int index, Consumer<T> consumer) {
+        if (index > lastUsed) {
+            return;
+        }
+        inOrderTraverse(index * 2, consumer);
+        consumer.accept((T)tree[index]);
+        inOrderTraverse(index * 2 + 1, consumer);
+    }
+
+    void postOrderTraverse(int index, Consumer<T> consumer) {
+        if (index > lastUsed) {
+            return;
+        }
+        postOrderTraverse(index * 2, consumer);
+        postOrderTraverse(index * 2 + 1, consumer);
+        consumer.accept((T)tree[index]);
+    }
+
+    void levelOrderTraverse(Consumer<T> consumer) {
+        for (int i = 1; i <= lastUsed; i++) {
+            consumer.accept((T)tree[i]);
+        }
     }
 
     @Override
     public boolean contains(T value) {
-        throw new UnsupportedOperationException();
+        requireNonNull(value, "value");
+        for (int i = 1; i <= lastUsed; i++) {
+            if (value.equals(tree[i])) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public void erase() {
-        throw new UnsupportedOperationException();
+        for (int i = 1; i <= lastUsed; i++) {
+            tree[i] = null;
+        }
+        lastUsed = 0;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException();
+        return lastUsed == 0;
+    }
+
+    private boolean isFull() {
+        return lastUsed == tree.length - 1;
     }
 
 }
--- src/main/java/tree/binary/LinkedListTree.java
+++ src/main/java/tree/binary/LinkedListTree.java
@@ -1,6 +1,10 @@
 package tree.binary;
 
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
 import java.util.function.Consumer;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * @author Dmitry Pavlov
@@ -8,38 +12,265 @@ import java.util.function.Consumer;
  */
 public class LinkedListTree<T> implements Tree<T> {
 
+    private Node<T> root;
+
     @Override
     public void insert(T value) {
-        throw new UnsupportedOperationException();
+
+        requireNonNull(value, "value");
+
+        if (isEmpty()) {
+            root = new Node<>(value, null, null, null);
+            return;
+        }
+
+        var queue = new LinkedList<Node<T>>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            var current = queue.remove();
+            if (current != null) {
+                if (current.getLeft() == null) {
+                    current.setLeft(new Node<>(value, current, null, null));
+                    return;
+                }
+                if (current.getRight() == null) {
+                    current.setRight(new Node<>(value, current, null, null));
+                    return;
+                }
+                queue.add(current.getLeft());
+                queue.add(current.getRight());
+            }
+        }
+
     }
 
     @Override
     public void delete(T value) {
-        throw new UnsupportedOperationException();
+
+        if (root == null) {
+            throw new IllegalStateException("Can not delete value from empty tree");
+        }
+
+        Node<T> nodeToDelete = null;
+
+        var queue = new LinkedList<Node<T>>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            var current = queue.remove();
+            if (current != null) {
+                if (current.getValue().equals(value)) {
+                    nodeToDelete = current;
+                    break;
+                }
+                queue.add(current.getLeft());
+                queue.add(current.getRight());
+            }
+        }
+
+        if (nodeToDelete == null) {
+            throw new NoSuchElementException("There is no such value in the tree");
+        }
+
+        if (nodeToDelete.isLeaf()) {
+            if (nodeToDelete.getParent() == null) { // root
+                erase();
+            } else {
+                deleteLeaf(nodeToDelete);
+            }
+            return;
+        }
+
+        queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            var current = queue.remove();
+            if (current != null) {
+                if (current.isLeaf()) {
+                    nodeToDelete.setValue(current.getValue());
+                    deleteLeaf(current);
+                    return;
+                }
+                queue.add(current.getLeft());
+                queue.add(current.getRight());
+            }
+        }
+
+    }
+
+    private void deleteLeaf(Node<T> node) {
+        if (!node.isLeaf()) {
+            throw new IllegalArgumentException("The node is not a leaf");
+        }
+        if (node.getParent().getLeft() == node) {
+            node.getParent().setLeft(null);
+        } else {
+            node.getParent().setRight(null);
+        }
+        node.setParent(null);
+
     }
 
     @Override
     public void traverse(Consumer<T> consumer, TraversalType traversalType) {
-        throw new UnsupportedOperationException();
+
+        requireNonNull(consumer, "consumer");
+        requireNonNull(traversalType, "traversalType");
+
+        switch (traversalType) {
+            case PRE_ORDER: {
+                preOrderTraverse(root, consumer);
+            }
+            break;
+            case IN_ORDER: {
+                inOrderTraverse(root, consumer);
+            }
+            break;
+            case POST_ORDER: {
+                postOrderTraverse(root, consumer);
+            }
+            break;
+            case LEVEL_ORDER: {
+                levelOrderTraverse(root, consumer);
+            }
+            break;
+            default: {
+                throw new IllegalArgumentException("This type of traversal is not supported");
+            }
+        }
+
     }
 
     public void traverse(Consumer<T> consumer) {
-        throw new UnsupportedOperationException();
+        traverse(consumer, TraversalType.LEVEL_ORDER);
+    }
+
+    void preOrderTraverse(Node<T> current, Consumer<T> consumer) {
+        if (current == null) {
+            return;
+        }
+        consumer.accept(current.getValue());
+        preOrderTraverse(current.getLeft(), consumer);
+        preOrderTraverse(current.getRight(), consumer);
+    }
+
+    void inOrderTraverse(Node<T> current, Consumer<T> consumer) {
+        if (current == null) {
+            return;
+        }
+        inOrderTraverse(current.getLeft(), consumer);
+        consumer.accept(current.getValue());
+        inOrderTraverse(current.getRight(), consumer);
+    }
+
+    void postOrderTraverse(Node<T> current, Consumer<T> consumer) {
+        if (current == null) {
+            return;
+        }
+        postOrderTraverse(current.getLeft(), consumer);
+        postOrderTraverse(current.getRight(), consumer);
+        consumer.accept(current.getValue());
+    }
+
+    void levelOrderTraverse(Node<T> root, Consumer<T> consumer) {
+        if (root == null) {
+            return;
+        }
+        var queue = new LinkedList<Node<T>>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            var current = queue.remove();
+            if (current != null) {
+                consumer.accept(current.getValue());
+                queue.add(current.getLeft());
+                queue.add(current.getRight());
+            }
+        }
     }
 
     @Override
     public boolean contains(T value) {
-        throw new UnsupportedOperationException();
+
+        requireNonNull(value, "value");
+
+        if (isEmpty()) {
+            return false;
+        }
+
+        var queue = new LinkedList<Node<T>>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            var current = queue.remove();
+            if (current != null) {
+                if (current.getValue().equals(value)) {
+                    return true;
+                }
+                queue.add(current.getLeft());
+                queue.add(current.getRight());
+            }
+        }
+        return false;
+
     }
 
     @Override
     public void erase() {
-        throw new UnsupportedOperationException();
+        root = null;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException();
+        return root == null;
+    }
+
+    public static class Node<T> {
+
+        private T value;
+        private Node<T> parent;
+        private Node<T> left;
+        private Node<T> right;
+
+        public Node(T value, Node<T> parent, Node<T> left, Node<T> right) {
+            this.value = requireNonNull(value, "value");
+            this.parent = parent;
+            this.left = left;
+            this.right = right;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+
+        public Node<T> getParent() {
+            return parent;
+        }
+
+        public void setParent(Node<T> parent) {
+            this.parent = parent;
+        }
+
+        public Node<T> getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node<T> left) {
+            this.left = left;
+        }
+
+        public Node<T> getRight() {
+            return right;
+        }
+
+        public void setRight(Node<T> right) {
+            this.right = right;
+        }
+
+        public boolean isLeaf() {
+            return left == null && right == null;
+        }
     }
 
 }
